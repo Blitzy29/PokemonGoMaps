c938b6ba05478fea370c09038b716d00ca7e7536
Yoann Péron <yoannperon29@gmail.com>
2017-02-08 15:16:25 +0100

    Setting the right column names

--- R/HeatMapPokemon.R
+++ R/HeatMapPokemon.R
@@ -2,16 +2,14 @@ p_load(R.PersonalTools)
 p_load(R.Maps)
 
 tablePokemonLocations <- fread("data/tablePokemonLocations.txt")
-setnames(tablePokemonLocations,c("latPokemon","longPokemon"),
-         c("latitude","longitude"))
 
-tablePokemonLocations[,nomPokemon] %>% table %>% sort
-tablePokemonLocations[,hour] %>% table %>% sort
+# tablePokemonLocations[,namePokemon] %>% table %>% sort
+# tablePokemonLocations[,hour] %>% table %>% sort
 
 pokemonChosen <- 'Zubat'
 
 map <- leaflet()
 map %<>% addTiles()
 map %<>% SetMapView('Paris')
-map %<>% AddHeatMap(tablePokemonLocations[nomPokemon == pokemonChosen & hour == hourPeriod])
+map %<>% AddHeatMap(tablePokemonLocations[namePokemon == pokemonChosen & hour == hourPeriod])
 DisplayMap(map)
